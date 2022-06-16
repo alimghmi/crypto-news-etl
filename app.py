84f8104b95e99519b6b3d2ab827966f2be437806
Ali Moghimi <mghmiali@gmail.com>
2022-06-16 17:44:14 +0430

    add app.py

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 import scraper
-import database
 import serializer
 
 
-def main():
-    pass
+def app():
+    engine = scraper.Scraper()
+    extracted = engine.run()
+    if not extracted:
+        print('Err', 'No data from scraper')
+        return False
+
+    print('Success', 'Data Extracted')
+
+    transofrm = serializer.Serializer(extracted)
+    if not transofrm.validate():
+        print('Err', 'Data validation failed')
+        return False
+
+    print('Success', 'Data validated')
+    output = transofrm.load()
+
+    print('Success', 'Data loaded to database')
+    print(output)
 
 
 if __name__ == '__main__':
-    main()
+    app()
