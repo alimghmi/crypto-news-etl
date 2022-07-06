d7db88e40c1deeb5759a69805ef0f9bc85e09a51
Ali Moghimi <mghmiali@gmail.com>
2022-07-06 16:14:03 +0430

    add sqlite seperate module

--- database/sqlite.py
+++ database/sqlite.py
@@ -1,14 +1,19 @@
 import sqlite3
 import sqlalchemy
 import pandas as pd
+from decouple import config
 
 
 class SQLLite:
+    
 
-    def __init__(self, name='db.sqlite3', table='news'):
-        self.engine = sqlalchemy.create_engine(f'sqlite:///{name}')
-        self.connection = sqlite3.connect(name)
-        self.table = table
+    NAME = config('SQLITE_DATABASE_NAME', default='db.sqlite3')
+
+
+    def __init__(self):
+        self.table = 'news'
+        self.engine = sqlalchemy.create_engine(f'sqlite:///{self.NAME}')
+        self.connection = sqlite3.connect(self.NAME)
         self.__create__()
 
     def __create__(self):
@@ -22,11 +27,10 @@ class SQLLite:
         cursor = self.connection.cursor()
         cursor.execute(query)
 
-    def to_db(self, df):
-        df.to_sql(self.table, self.engine, index=False, if_exists='append')
+    def to_db(self, dataframe):
+        dataframe.to_sql(self.table, self.engine, index=False, if_exists='append')
         self.connection.close()
 
     def get_table(self):
         return pd.read_sql(self.table, self.engine, 
             columns=['title', 'url', 'timestamp'], parse_dates=['timestamp'])
-    
